8ccb0ae9fbabf4adc63a556d6a7c32ae8b35c921
Polina <polya3hus@gmail.com>
2024-02-07 14:47:26 -0600

    US_302/304

---
+++ US_302.java
@@ -0,0 +1,58 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class US_302 {
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://shopdemo.e-junkie.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @Test
+    public void testEJunkie() throws InterruptedException {
+        // Find and click the "Add Demo E-Book to Cart" button using XPath
+        WebElement addToCartButton = driver.findElement(By.xpath("//div[2]//a[1]//div[1]//div[2]//button[1]"));
+        addToCartButton.click();
+
+        WebElement iFrameElement=driver.findElement(By.xpath("(//iframe[@class='EJIframeV3 EJOverlayV3'])[1]"));
+        driver.switchTo().frame(iFrameElement);
+        Thread.sleep(2000);
+
+        // Wait for the cart to open and the Pay with debit card button to become clickable
+        WebElement payByDebitCard = driver.findElement(By.xpath("//*[@class='Payment-Button CC']"));
+        payByDebitCard.click();
+
+
+        // Attempt to pay with blank information
+        WebElement payButton = driver.findElement(By.xpath("//button[text()='Pay 0.50 USD']"));
+        payButton.click();
+
+
+        // Check for error messages using XPath
+          WebElement emailError = driver.findElement(By.id("SnackBar"));
+
+        Assert.assertTrue(emailError.isDisplayed());
+        }
+
+
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
+
---
+++ US_307.java
@@ -0,0 +1,50 @@
+import org.junit.Assert;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class US_307 {
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://shopdemo.e-junkie.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @Test
+    public void testNavigateToEJunkieHomepage() throws InterruptedException {
+
+        // Scroll to the "E-Commerce By E-Junkie" link and click it
+        WebElement ecommerceLink = driver.findElement(By.xpath("/html/body/div[7]/div/div/div[2]/a"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", ecommerceLink);
+        Thread.sleep(500);
+        ecommerceLink.click();
+
+        // Click on the e-junkie logo
+        WebElement eJunkieLogo = driver.findElement(By.xpath("//*[@id=\"top\"]/header/div/div/div[1]/a/img"));
+        eJunkieLogo.click();
+
+        // Verify the URL
+        String expectedUrl = "https://www.e-junkie.com/";
+        String actualUrl = driver.getCurrentUrl();
+
+        Assert.assertEquals("The URL did not match the expected page", expectedUrl, actualUrl);
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
